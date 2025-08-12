bbaf8fba083a15e0873b4e3dd70711860b2e017e
admin-mahboob <mahboobk522@gmail.com>
2025-08-12 09:17:42 +0500

    Saving V code 24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "emojibattery.widget.statusbar"
         minSdk = 24
         targetSdk = 35
-        versionCode = 23
-        versionName = "3.3"
+        versionCode = 24
+        versionName = "3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
